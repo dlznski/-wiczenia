d255b330e76c7de01c1d6837dbd0ce118cf75af9
dlznski <dluzniewskiamadeusz@gmail.com>
2023-04-25 18:02:40 +0200

    Plik skrypt.sh od teraz pokazuje aktualną datę lub tworzy 100 plików log zawierających aktualną datę oraz nazwę pliku, który go utworzył

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
-echo "Dzisiejsza data: $(date +%Y-%m-%d)"
+if [ "$1" = "--dane" ]; then
+    echo "Dzisiejsza data: $(date +%Y-%m-%d)"
+    exit 0
+fi
+
+if [ "$1" = "--logs" ]; then
+    for ((i=1; i<=100; i++))
+    do
+    plik="log$i.txt"
+    echo "Nazwa pliku: $plik" > $plik
+    echo "Nazwa skryptu: $0" >> $plik
+    echo "Data utworzenia: $(date)" >> $plik
+    done
+    exit 0
+fi
